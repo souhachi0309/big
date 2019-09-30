c8463d4db79377976f3b3d51e337ad5c9d03fcc6
htc-mmellali <54902709+htc-mmellali@users.noreply.github.com>
2019-09-30 17:23:32 +0200

    Create MonPremierProgramme.java

---
+++ java-standard/MonPremierProgramme.java
@@ -0,0 +1,13 @@
+package fr.htc.java.oo;
+
+public class MonPremierProgramme {
+
+	public static void main(String[] args) {
+		Circle c; // C est une référence sur un objet de type Circle, par encore un objet
+		c = new Circle(5.0); // c référence maintenant un objet alloué en mémoire
+		c.x = c.y = 10.0;
+		System.out.println("Aire du circle est : " + c.area());
+
+	}
+
+}
