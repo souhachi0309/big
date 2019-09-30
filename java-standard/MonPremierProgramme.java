16eacf560b218df9b3dcecb1fc33c30671cb4b1b
htc-mmellali <54902709+htc-mmellali@users.noreply.github.com>
2019-09-30 17:24:26 +0200

    Update MonPremierProgramme.java

--- java-standard/MonPremierProgramme.java
+++ java-standard/MonPremierProgramme.java
@@ -5,7 +5,7 @@ public class MonPremierProgramme {
 	public static void main(String[] args) {
 		Circle c; // C est une référence sur un objet de type Circle, par encore un objet
 		c = new Circle(5.0); // c référence maintenant un objet alloué en mémoire
-		c.x = c.y = 10.0;
+		c.x = c.y = 10.0; // ces valeurs sont stocké dans le corps de l'objet
 		System.out.println("Aire du circle est : " + c.area());
 
 	}
